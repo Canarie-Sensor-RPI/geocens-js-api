e24e4cf056f9000850450b1511ec999abc51dd82
James Badger <jamesbadger@gmail.com>
2013-06-07 14:05:19 -0600

    Add noConflict mode for Geocens

--- geocens.js
+++ geocens.js
@@ -9,6 +9,9 @@
   // Reference to global object
   var root = this;
 
+  // Save previous value of Geocens
+  var previousGeocens = root.Geocens;
+
   // Set top-level namespace
   var Geocens;
   Geocens = root.Geocens = {};
@@ -18,5 +21,12 @@
 
   // Require jQuery into `$` variable
   Geocens.$ = root.jQuery || root.$;
+
+  // Run Geocens in noConflict mode, which prevents Geocens from overwriting
+  // whatever previously held the `Geocens` variable.
+  Geocens.noConflict = function() {
+    root.Geocens = previousGeocens;
+    return this;
+  };
 
 }).call(this);
---
+++ test/noConflict.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+  test('noConflict', 2, function() {
+    var noConfictGeocens = Geocens.noConflict();
+    equal(window.Geocens, undefined, 'Returned window.Geocens');
+    window.Geocens = noConfictGeocens;
+    equal(window.Geocens, noConfictGeocens, 'Geocens is still pointing to the original Geocens');
+  });
+
+});
