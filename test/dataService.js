dc6bfa314dfdba24f57b9e421d90c9e0afc0d15c
James Badger <jamesbadger@gmail.com>
2013-06-10 16:47:55 -0600

    Set parent reference for alternate method

--- test/dataService.js
+++ test/dataService.js
@@ -249,6 +249,41 @@ $(document).ready(function() {
 
 });
 
+test('sets the `service` property for the Sensor returned', 1, function() {
+    var api_key = "your_32_character_api_key",
+        sensor_id = "32_character_sensor_id",
+        datastream_id = "32_character_datastream_id";
+
+    var server = this.sandbox.useFakeServer();
+    var basePath = Geocens.DataService.path;
+    var path = basePath + "sensors/" + sensor_id + "/datastreams/" + datastream_id;
+
+    server.respondWith("GET", path,
+                       [200, { "Content-Type": "application/json" },
+                        JSON.stringify(Fixtures.DataService.Sensor)]);
+
+    var newSensor;
+
+    // Retrieve sensor
+    var source = new Geocens.DataService({
+      api_key: api_key
+    });
+
+    source.getSensor({
+      sensor_id: sensor_id,
+      datastream_id: datastream_id,
+      done: function (sensor) {
+        newSensor = sensor;
+      }
+    });
+
+    server.respond();
+
+    ok(newSensor.service !== undefined, "Sensor service was not defined");
+
+    server.restore();
+  });
+
 // Fixtures
 
 window.Fixtures = {
