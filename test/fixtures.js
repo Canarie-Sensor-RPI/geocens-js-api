b4f29c39a3f6cba588e55ffe483b8dea7424f972
James Badger <jamesbadger@gmail.com>
2013-06-10 16:56:40 -0600

    Move fixtures to own file

---
+++ test/fixtures.js
@@ -0,0 +1,18 @@
+// Fixtures
+
+window.Fixtures = {
+  DataService: {
+    Sensor: {
+      datastream_id: "4e1552900482a04b8850e4c4b097b690",
+      latitude: 51.000,
+      longitude: -114.000,
+      property: "airquality",
+      sensor_id: "d0bcf2894edc50a5160c52a4015bc768",
+      sensor_type: "DataService",
+      service_url: "http://example.com/data_service",
+      unit: "ppm",
+      user: "bob@example.com",
+      user_id: "4b9bb80620f03eb3719e0a061c14283d"
+    }
+  }
+};
