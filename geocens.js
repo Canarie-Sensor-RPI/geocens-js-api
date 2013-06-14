3b2e8d1eba9eeefea2a00b85eb98a7d161966e29
James Badger <jamesbadger@gmail.com>
2013-06-14 15:33:58 -0600

    Add Datastream object

--- geocens.js
+++ geocens.js
@@ -29,6 +29,86 @@
     return this;
   };
 
+  // Geocens.Datastream
+  // ------------------
+  //
+  var Datastream = Geocens.Datastream = function(options) {
+    // Handle undefined options
+    if (options === undefined) {
+      options = {};
+    }
+
+    this._attributes = options;
+    this.sensor_id = options.sensor_id;
+    this.datastream_id = options.datastream_id;
+    this._data = [];
+  };
+
+  // Extend Datasteam object (actually function) prototype with new methods and
+  // properties
+  jQuery.extend(Datastream.prototype, {
+    // Respond to call for attributes
+    attributes: function() {
+      return this._attributes;
+    },
+
+    // Merge an array into currently cached time series data for the datastream
+    cache: function(data) {
+      this._data = this._data.concat(data);
+
+      // sort by timestamp
+      this._data.sort(function(a, b) {
+        if (a.timestamp < b.timestamp) {
+          return -1;
+        } else if (a.timestamp > b.timestamp) {
+          return 1;
+        } else {
+          return 0;
+        }
+      });
+
+      return this._data;
+    },
+
+    // Retrieve time series data for the datastream
+    getTimeSeries: function(options) {
+      var datastream = this;
+
+      if (options === undefined) {
+        options = {};
+      }
+
+      options.done = options.done || function() {};
+
+      var path = this.service.path + "datastreams/" + this.datastream_id + "/records";
+
+      $.ajax({
+        url: path,
+        type: 'GET',
+        headers: {
+          "x-api-key": options.api_key || this.service.api_key
+        }
+      }).done(function (data) {
+        var convertedData = $.map(data, function(value, index) {
+          return {
+            timestamp: Date.parse(value.id),
+            value: parseFloat(value.reading)
+          };
+        });
+
+        datastream.cache(convertedData);
+        options.done(convertedData);
+      });
+
+    },
+
+    // Return cached time series data
+    seriesData: function() {
+      return this._data;
+    }
+  });
+
+  // REMOVE
   // Geocens.Sensor
   // --------------
   //
