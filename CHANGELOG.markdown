7feab0ca73373a5a87d36cacdf542794e9db7387
James Badger <jamesbadger@gmail.com>
2013-06-14 15:43:08 -0600

    Bump version to 0.2

--- CHANGELOG.markdown
+++ CHANGELOG.markdown
@@ -1,3 +1,6 @@
+0.2 (June 14 2013)
+* Change semantics to use "Datastream" instead of "Sensor", reflecting GeoCENS Data Service REST API
+
 0.1 (June 14 2013)
 * Basic support for GeoCENS Data Service
 
--- geocens.js
+++ geocens.js
@@ -1,4 +1,4 @@
-//    GeoCENS.js 0.1
+//    GeoCENS.js 0.2
 
 //    (c) 2013, James Badger, Geo Sensor Web Lab.
 //    All Rights Reserved.
@@ -17,7 +17,7 @@
   Geocens = root.Geocens = {};
 
   // Current library version
-  Geocens.VERSION = '0.1';
+  Geocens.VERSION = '0.2';
 
   // Require jQuery into `$` variable
   Geocens.$ = root.jQuery || root.$;
