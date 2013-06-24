d11daa721f5fc73cbbb3f4c6e4be6f3ec5754b7d
James Badger <jamesbadger@gmail.com>
2013-06-24 11:30:25 -0600

    Attributes stores init options

--- test/observation.js
+++ test/observation.js
@@ -7,4 +7,16 @@ $(document).ready(function() {
     ok(observation.attributes() instanceof Object, "attributes is not object");
   });
 
+  test("attributes returns initialization options", 2, function() {
+    var options = {
+      offering: "Temperature",
+      property: "urn:ogc:def:property:noaa:ndbc:Water Temperature"
+    };
+    var observation = new Geocens.Observation(options);
+    var attributes = observation.attributes();
+
+    equal(attributes.offering, options.offering);
+    equal(attributes.property, options.property);
+  });
+
 });
