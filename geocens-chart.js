a6b58909f5b6bc0bf95ebba663f4f9d2debcabd5
James Badger <jamesbadger@gmail.com>
2013-06-25 17:22:10 -0600

    Fix chart call usage, add basic graphing

--- geocens-chart.js
+++ geocens-chart.js
@@ -9,12 +9,70 @@
       chart,
       defaults;
 
-  defaults = root.defaults = {};
+  defaults = root.defaults = {
+    chart: {}
+  };
 
   chart = root.chart = function(element, options) {
     var settings = $.extend({}, defaults, options);
 
-    return this;
+    var datasource = settings.observation || settings.datastream;
+
+    if (datasource.seriesData().length === 0) {
+      console.warn("Warning: trying to graph empty time series");
+    }
+
+    var seriesData = $.map(datasource.seriesData(), function(datapoint) {
+      return [[datapoint.timestamp, datapoint.value]];
+    });
+
+    var stockChart = new Highcharts.StockChart({
+      chart: {
+         renderTo: element
+      },
+
+      title: {
+       text: datasource._attributes.id
+      },
+
+      tooltip: {
+          formatter: function () {
+              var point = this.points[0];
+              var series = point.series;
+              var format = '%A, %b %e, %Y, %H:%M:%S';
+
+              if (series.tooltipHeaderFormat) {
+                  format = series.tooltipHeaderFormat;
+              }
+
+              return "<strong>" + Highcharts.dateFormat(format, this.x) +
+                     "</strong><br>" + series.name + ":" + point.y + " " +
+                     datasource._attributes.unit;
+          }
+      },
+
+      rangeSelector: {
+          enabled: false
+      },
+
+      series: [{
+          name: datasource._attributes.id,
+          data: seriesData
+      }],
+
+      xAxis: {
+          type: 'datetime'
+      },
+
+      yAxis: {
+          offset: 50,
+          title: {
+              text: datasource._attributes.unit
+          }
+      }
+    });
+
+    return stockChart;
   };
 
   $.fn.GeocensChart = function () {
