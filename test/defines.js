51ff6f7797721c67507ff6971ff1165ba1c0ae2a
James Badger <jamesbadger@gmail.com>
2013-10-18 13:54:10 -0600

    Remove jQuery test

--- test/defines.js
+++ test/defines.js
@@ -4,9 +4,4 @@ $(document).ready(function() {
     ok(Geocens !== undefined, "Geocens is not defined");
   });
 
-  test('includes jQuery', 2, function() {
-    ok(Geocens.$ !== undefined, "Geocens.$ is not defined");
-    ok(Geocens.$ === jQuery, "Geocens.$ is not jQuery");
-  });
-
 });
